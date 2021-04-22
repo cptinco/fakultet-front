61ae119dff16f1132bff332e64b60a831c28a22e
cptinco <dominik.user@gmail.com>
2021-04-22 12:26:19 +0200

    Poczatki komponentow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import KartaPacjenta from './components/KartaPacjenta';
+import KartaLekarza from './components/KartaLekarza';
 
+const datefromdb = [
+  {
+    imie: "jan", numer: 5
+  },
+  {
+    imie: "andrzej", numer: 4
+  }
+]
+const datefromdb2 = [
+  {
+    imie: "kuba", nazwisko: "jakis"
+  },
+  {
+    imie: "jarek", nazwisko: "takis"
+  }
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        datefromdb.map(osoba=>(
+          <KartaPacjenta imie={osoba.imie} numer={osoba.numer}/>
+        ))
+      }
+      {
+         datefromdb2.map(lekarz=>(
+          <KartaLekarza imie={lekarz.imie} nazwisko={lekarz.nazwisko}/>
+         ))
+      }
     </div>
   );
 }
